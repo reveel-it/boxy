2e83a21fec9040c9388bc2271ff71996a2a9a684
azw1998 <azw1998@gmail.com>
2026-03-31 11:46:17 -0700

    get active session

--- reveel_lib/shipment.py
+++ reveel_lib/shipment.py
@@ -1,6 +1,10 @@
 import snowflake.snowpark.functions as F
 from snowflake.snowpark import DataFrame
 
+from snowflake.snowpark.context import get_active_session
+
+session = get_active_session()
+
 
 def get_shipment(tracking_number: str) -> DataFrame:
     return session.table("staging.charge.premodel").where(
